91e214de13764d503b90967cd70f895fdc77aa26
omar.aboghadeer <omar.abo.ghadeer@hotmail.com>
2022-03-13 00:49:09 +0200

    init - and creat header - button components

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button 
+        :style="{background: color}" 
+        class="btn"
+        @click="onClick()"
+    >
+    {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+            console.log('clicked')
+        }
+    }
+}
+</script>
