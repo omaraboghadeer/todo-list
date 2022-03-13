b4f6e54c3e7bc1ec42b784378a6517c8f3db7759
omar.aboghadeer <omar.abo.ghadeer@hotmail.com>
2022-03-13 02:30:01 +0200

    install json-server as a backend apis, fetching the tasks, adding new task

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,19 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+
+    async addTask(task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data]
     },
 
     toggleReminder(id) {
@@ -48,30 +59,29 @@ export default {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
       }
+    },
+
+    // ------------ APIs ------------
+
+    // Fetching Tasks
+    async fetchTasks() {
+      const res = await fetch('api/tasks');
+      const data = await res.json();
+      return data;
+    },
+
+    // fetching task with id
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`);
+      const data = await res.json();
+      return data;
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'March 2nd at 1:30pm',
-        reminder: true
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false
-      },
-    ]
+
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
+
 }
 </script>
 
