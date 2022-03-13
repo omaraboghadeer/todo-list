25605a091e0e8f8d22f97ee43f89e0375c76b76f
omar.aboghadeer <omar.abo.ghadeer@hotmail.com>
2022-03-13 02:42:55 +0200

    make toggleReminder - deleteTask - addTask working with API methods DELETE UPDATEPOST

--- src/App.vue
+++ src/App.vue
@@ -51,13 +51,30 @@ export default {
       this.tasks = [...this.tasks, data]
     },
 
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder};
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask)
+      });
+
+      const data = res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !data.reminder} : task)
     },
 
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
       }
     },
 
